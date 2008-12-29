0919b156cb44b92c7a3d29c91e8e283ffe91b67d
kapitan.drake <kapitan.drake@be961cae-a4ca-11dd-adfd-a1ec4d499d49>
2008-12-29 18:12:30 +0000

    Added preliminary Avisynth (*.avs files) scripting support (not supported in 'Multiple mono input' and in 'One per file' mode). Added *.avs filter to open dialog. Double-click on *.avs script in files list to show AVS File Properties window. Moved to development of version 3.0 (current status is BETA).

---
+++ trunk/src/Avs2Raw.h
@@ -0,0 +1,67 @@
+﻿//
+// WAV to AC3 Encoder
+// Copyright (C) 2007, 2008, 2009 Wiesław Šoltés <wieslaw.soltes@gmail.com>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; version 2 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+/* $Id$ */
+
+#pragma once
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <io.h>
+
+// include Avisynth headers
+#include "avisynth\src\internal.h"
+
+typedef struct TAvsAudioInfo
+{
+    int nSampleType;
+    int nSamplesPerSecond;
+    int nAudioChannels;
+    __int64 nAudioSamples;
+    int nBytesPerChannelSample;
+} AvsAudioInfo, *PAvsAudioInfo;
+
+typedef struct TAvs2RawStatus
+{
+    __int64 nStart;
+    __int64 nSamples;
+    __int64 nSamplesLeft;
+    int nSamplesToRead;
+} Avs2RawStatus, *PAvs2RawStatus;
+
+class CAvs2Raw
+{
+public:
+    CAvs2Raw();
+    virtual ~CAvs2Raw();
+private:
+    HINSTANCE hAvisynthDLL;
+    IScriptEnvironment* (__stdcall *CreateEnv) (int);
+    IScriptEnvironment *env;
+    PClip *Video;
+private:
+    AvsAudioInfo info;
+    const char *_szAvsFileName;
+    bool bHaveDLL;
+public:
+    bool OpenAvisynth(const char *szAvsFileName);
+    bool CloseAvisynth();
+public:
+    AvsAudioInfo GetInputInfo();
+    int GetAudio(void* pBuffer, Avs2RawStatus *pStatus);
+};
--- trunk/src/EncWAVtoAC3.cpp
+++ trunk/src/EncWAVtoAC3.cpp
@@ -1,6 +1,6 @@
 ﻿//
 // WAV to AC3 Encoder
-// Copyright (C) 2007-2008 Wiesław Šoltés <wieslaw.soltes@gmail.com>
+// Copyright (C) 2007, 2008, 2009 Wiesław Šoltés <wieslaw.soltes@gmail.com>
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -50,7 +50,7 @@ const CString szHelpMessage =
 	_T("Do not save configuration on exit.\n")
     _T("\n")
     _T("-i, --input-file <file.ext>\n")
-	_T("Set input files path (*.wav, *.bin, *.pcm, *.raw).\n")
+	_T("Set input files path (*.wav, *.bin, *.pcm, *.raw, *.avs).\n")
 	_T("\n")
     _T("-o, --output-file <file.ext>\n")
 	_T("Set output file path (*.ac3).\n")
