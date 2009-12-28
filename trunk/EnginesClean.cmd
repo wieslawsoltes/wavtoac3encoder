fec108ad1d944ba528ba28403f8e6e827fcf4dad
wieslaw.soltes <wieslaw.soltes@be961cae-a4ca-11dd-adfd-a1ec4d499d49>
2009-12-28 23:05:13 +0000

    Updated EnginesClean.cmd batch script.

--- trunk/EnginesClean.cmd
+++ trunk/EnginesClean.cmd
@@ -37,16 +37,14 @@ del "%BasePath%\%EnginesFileName%"
 
 set BasePath=Debug\x64
 rmdir /S /Q "%BasePath%\libaftendll_AMD64"
-rmdir /S /Q "%BasePath%\libaftendll_AMD6_SSE"
-rmdir /S /Q "%BasePath%\libaftendll_AMD6_SSE2"
-rmdir /S /Q "%BasePath%\libaftendll_AMD6_SSE3"
+rmdir /S /Q "%BasePath%\libaftendll_AMD64_SSE2"
+rmdir /S /Q "%BasePath%\libaftendll_AMD64_SSE3"
 del "%BasePath%\%EnginesFileName%"
 
 set BasePath=Release\x64
-rmdir /S /Q "%BasePath%\libaftendll_AMD6"
-rmdir /S /Q "%BasePath%\libaftendll_AMD6_SSE"
-rmdir /S /Q "%BasePath%\libaftendll_AMD6_SSE2"
-rmdir /S /Q "%BasePath%\libaftendll_AMD6_SSE3"
+rmdir /S /Q "%BasePath%\libaftendll_AMD64"
+rmdir /S /Q "%BasePath%\libaftendll_AMD64_SSE2"
+rmdir /S /Q "%BasePath%\libaftendll_AMD64_SSE3"
 del "%BasePath%\%EnginesFileName%"
 
 
