e917fe9b8ec66b283d3828543211126b08e50d69
wieslaw.soltes <wieslaw.soltes@be961cae-a4ca-11dd-adfd-a1ec4d499d49>
2010-07-27 18:31:18 +0000

    Updated copyright headers.

--- installer/PackageBinariesAMD64.cmd
+++ installer/PackageBinariesAMD64.cmd
@@ -1,7 +1,7 @@
 @echo off
 rem
 rem WAV to AC3 Encoder
-rem Copyright (C) 2007, 2008, 2009 Wieslaw Soltes <wieslaw.soltes@gmail.com>
+rem Copyright (C) 2007,2008,2009,2010 Wieslaw Soltes <wieslaw.soltes@gmail.com>
 rem
 rem This program is free software; you can redistribute it and/or modify
 rem it under the terms of the GNU General Public License as published by
