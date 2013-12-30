6331636310feb75b95c76573002d241d087d1a51
wieslaw.soltes@gmail.com <wieslaw.soltes@gmail.com@be961cae-a4ca-11dd-adfd-a1ec4d499d49>
2013-12-30 19:44:19 +0000

    Re-factored EncWAVtoAC3WorkDlg code

--- src/EncWAVtoAC3WorkDlg.cpp
+++ src/EncWAVtoAC3WorkDlg.cpp
@@ -83,13 +83,82 @@ BOOL CEncWAVtoAC3WorkDlg::OnInitDialog()
 {
     CDialogEx::OnInitDialog();
 
-    // setup worker thread settings
-    workParam.pWorkDlg = this;
+	InitSettings();
+	InitCtrls();
+	CreateWorker();
 
-	if(this->workParam.bMultiMonoInput == false)
+    return TRUE;
+}
+
+void CEncWAVtoAC3WorkDlg::OnClose()
+{
+    // stop file timer
+    KillTimer(WM_FILE_TIMER);
+
+    // stop total timer
+    KillTimer(WM_TOTAL_TIMER);
+
+    // stop encoding process and close work dialog
+    if(this->bTerminate == false)
+    {
+        this->bTerminate = true;
+        return;
+    }
+    else
+        this->EndDialog(IDOK);
+
+    CDialogEx::OnClose();
+}
+
+void CEncWAVtoAC3WorkDlg::OnDestroy()
+{
+    CDialogEx::OnDestroy();
+
+    if(this->hThread != NULL)
+    {
+        ::TerminateThread(this->hThread, 0);
+        ::CloseHandle(this->hThread);
+        this->hThread = NULL;
+    }
+}
+
+void CEncWAVtoAC3WorkDlg::OnTimer(UINT_PTR nIDEvent)
+{
+    switch(nIDEvent)
+    {
+    case WM_TOTAL_TIMER: 
+		UpdateTotalTimer();
+		return;
+    case WM_FILE_TIMER: 
+		UpdateFileTimer(); 
+		return;
+    };
+
+    CDialogEx::OnTimer(nIDEvent);
+}
+
+void CEncWAVtoAC3WorkDlg::OnBnClickedCancel()
+{
+    // stop encoding process and close work dialog
+    if(this->bTerminate == false)
+        this->bTerminate = true;
+    else
+        this->EndDialog(IDOK);
+
+    // OnCancel();
+}
+
+void CEncWAVtoAC3WorkDlg::InitSettings()
+{
+	workParam.pWorkDlg = this;
+}
+
+void CEncWAVtoAC3WorkDlg::InitCtrls()
+{
+	if (this->workParam.bMultiMonoInput == false)
 	{
 		// hide unused static controls
-		for(int i = 1; i < 6; i++)
+		for (int i = 1; i < 6; i++)
 		{
 			this->GetDlgItem(nIDIn[i])->ShowWindow(SW_HIDE);
 			this->GetDlgItem(nIDInInfo[i])->ShowWindow(SW_HIDE);
@@ -105,7 +174,7 @@ BOOL CEncWAVtoAC3WorkDlg::OnInitDialog()
 		CRect rcGroup, rcGroupStats, rcBtnCancel;
 		CRect rcDlg;
 
-		for(int i = 0; i < NUM_MAX_INPUT_FILES; i++)
+		for (int i = 0; i < NUM_MAX_INPUT_FILES; i++)
 		{
 			this->GetDlgItem(this->nIDIn[i])->GetWindowRect(rcIn[i]);
 			this->GetDlgItem(this->nIDInInfo[i])->GetWindowRect(rcInInfo[i]);
@@ -171,44 +240,44 @@ BOOL CEncWAVtoAC3WorkDlg::OnInitDialog()
 		this->ScreenToClient(rcBtnCancel);
 		this->m_BtnCancel.MoveWindow(rcBtnCancel);
 
-        rcSpeedInfo[0][0].MoveToY(rcSpeedInfo[0][0].top - nHeight);
-        this->ScreenToClient(rcSpeedInfo[0][0]);
+		rcSpeedInfo[0][0].MoveToY(rcSpeedInfo[0][0].top - nHeight);
+		this->ScreenToClient(rcSpeedInfo[0][0]);
 		this->GetDlgItem(IDC_STATIC_ENCODER_LABEL)->MoveWindow(rcSpeedInfo[0][0]);
 
-        rcSpeedInfo[0][1].MoveToY(rcSpeedInfo[0][1].top - nHeight);
-        this->ScreenToClient(rcSpeedInfo[0][1]);
+		rcSpeedInfo[0][1].MoveToY(rcSpeedInfo[0][1].top - nHeight);
+		this->ScreenToClient(rcSpeedInfo[0][1]);
 		this->GetDlgItem(IDC_STATIC_ENCODER_UNIT)->MoveWindow(rcSpeedInfo[0][1]);
 
-        rcSpeedInfo[0][2].MoveToY(rcSpeedInfo[0][2].top - nHeight);
-        this->ScreenToClient(rcSpeedInfo[0][2]);
+		rcSpeedInfo[0][2].MoveToY(rcSpeedInfo[0][2].top - nHeight);
+		this->ScreenToClient(rcSpeedInfo[0][2]);
 		this->GetDlgItem(IDC_STATIC_ENCODER_SPEED_AVG)->MoveWindow(rcSpeedInfo[0][2]);
 
-        rcSpeedInfo[1][0].MoveToY(rcSpeedInfo[1][0].top - nHeight);
-        this->ScreenToClient(rcSpeedInfo[1][0]);
+		rcSpeedInfo[1][0].MoveToY(rcSpeedInfo[1][0].top - nHeight);
+		this->ScreenToClient(rcSpeedInfo[1][0]);
 		this->GetDlgItem(IDC_STATIC_READS_LABEL)->MoveWindow(rcSpeedInfo[1][0]);
 
-        rcSpeedInfo[1][1].MoveToY(rcSpeedInfo[1][1].top - nHeight);
-        this->ScreenToClient(rcSpeedInfo[1][1]);
+		rcSpeedInfo[1][1].MoveToY(rcSpeedInfo[1][1].top - nHeight);
+		this->ScreenToClient(rcSpeedInfo[1][1]);
 		this->GetDlgItem(IDC_STATIC_READS_UNIT)->MoveWindow(rcSpeedInfo[1][1]);
 
-        rcSpeedInfo[1][2].MoveToY(rcSpeedInfo[1][2].top - nHeight);
-        this->ScreenToClient(rcSpeedInfo[1][2]);
+		rcSpeedInfo[1][2].MoveToY(rcSpeedInfo[1][2].top - nHeight);
+		this->ScreenToClient(rcSpeedInfo[1][2]);
 		this->GetDlgItem(IDC_STATIC_READS_SPEED_AVG)->MoveWindow(rcSpeedInfo[1][2]);
 
-        rcSpeedInfo[2][0].MoveToY(rcSpeedInfo[2][0].top - nHeight);
-        this->ScreenToClient(rcSpeedInfo[2][0]);
+		rcSpeedInfo[2][0].MoveToY(rcSpeedInfo[2][0].top - nHeight);
+		this->ScreenToClient(rcSpeedInfo[2][0]);
 		this->GetDlgItem(IDC_STATIC_WRITES_LABEL)->MoveWindow(rcSpeedInfo[2][0]);
 
-        rcSpeedInfo[2][1].MoveToY(rcSpeedInfo[2][1].top - nHeight);
-        this->ScreenToClient(rcSpeedInfo[2][1]);
+		rcSpeedInfo[2][1].MoveToY(rcSpeedInfo[2][1].top - nHeight);
+		this->ScreenToClient(rcSpeedInfo[2][1]);
 		this->GetDlgItem(IDC_STATIC_WRITES_UNIT)->MoveWindow(rcSpeedInfo[2][1]);
 
-        rcSpeedInfo[2][2].MoveToY(rcSpeedInfo[2][2].top - nHeight);
-        this->ScreenToClient(rcSpeedInfo[2][2]);
+		rcSpeedInfo[2][2].MoveToY(rcSpeedInfo[2][2].top - nHeight);
+		this->ScreenToClient(rcSpeedInfo[2][2]);
 		this->GetDlgItem(IDC_STATIC_WRITES_SPEED_AVG)->MoveWindow(rcSpeedInfo[2][2]);
 
-        rcGroupStats.MoveToY(rcGroupStats.top - nHeight);
-        this->ScreenToClient(rcGroupStats);
+		rcGroupStats.MoveToY(rcGroupStats.top - nHeight);
+		this->ScreenToClient(rcGroupStats);
 		this->GetDlgItem(IDC_STATIC_GROUP_STATS)->MoveWindow(rcGroupStats);
 
 		rcGroup.bottom -= nHeight;
@@ -218,161 +287,106 @@ BOOL CEncWAVtoAC3WorkDlg::OnInitDialog()
 		rcDlg.bottom -= nHeight;
 		this->MoveWindow(rcDlg);
 	}
+}
 
-	// create  worker thread
-	this->hThread = ::CreateThread(NULL, 
-		0, 
-		EncWorkThread, 
-		&workParam, 
-		0, 
+void CEncWAVtoAC3WorkDlg::UpdateTotalTimer()
+{
+	TCHAR strTime[32] = _T("");
+	m_ElapsedTimeTotal += 0.25;
+
+	// get time in format hh:mm:ss
+	if (m_ElapsedTimeTotal <= 59)
+	{
+		_stprintf(strTime, _T("%s 00:00:%02u\0"),
+			_T("Total elapsed time:"),
+			(unsigned long)m_ElapsedTimeTotal); // ss
+	}
+	else if (m_ElapsedTimeTotal <= 3599)
+	{
+		_stprintf(strTime, _T("%s 00:%02u:%02u\0"),
+			_T("Total elapsed time:"),
+			((unsigned long)m_ElapsedTimeTotal / 60), // mm
+			((unsigned long)m_ElapsedTimeTotal % 60)); // ss
+	}
+	else
+	{
+		_stprintf(strTime, _T("%s %02u:%02u:%02u\0"),
+			_T("Total elapsed time:"),
+			((unsigned long)m_ElapsedTimeTotal / 60) / 60, // hh
+			((unsigned long)m_ElapsedTimeTotal / 60) % 60, // mm
+			((((unsigned long)m_ElapsedTimeTotal / 60) % 60) * 60) % 60); // ss
+	}
+
+	// write to dialog
+	if (this->bCanUpdateWindow == true)
+	{
+		this->bCanUpdateWindow = false;
+		m_StcTimeTotal.SetWindowText(strTime);
+		this->bCanUpdateWindow = true;
+	}
+}
+
+void CEncWAVtoAC3WorkDlg::UpdateFileTimer()
+{
+	TCHAR strTime[32] = _T("");
+	m_ElapsedTimeFile += 0.25;
+
+	// get time in format hh:mm:ss
+	if (m_ElapsedTimeFile <= 59)
+	{
+		_stprintf(strTime, _T("%s 00:00:%02u\0"),
+			_T("Elapsed time:"),
+			(unsigned long)m_ElapsedTimeFile); // ss
+	}
+	else if (m_ElapsedTimeFile <= 3599)
+	{
+		_stprintf(strTime, _T("%s 00:%02u:%02u\0"),
+			_T("Elapsed time:"),
+			((unsigned long)m_ElapsedTimeFile / 60), // mm
+			((unsigned long)m_ElapsedTimeFile % 60)); // ss
+	}
+	else
+	{
+		_stprintf(strTime, _T("%s %02u:%02u:%02u\0"),
+			_T("Elapsed time:"),
+			((unsigned long)m_ElapsedTimeFile / 60) / 60, // hh
+			((unsigned long)m_ElapsedTimeFile / 60) % 60, // mm
+			((((unsigned long)m_ElapsedTimeFile / 60) % 60) * 60) % 60); // ss
+	}
+
+	// write to dialog
+	if (this->bCanUpdateWindow == true)
+	{
+		this->bCanUpdateWindow = false;
+
+		// show current time
+		m_StcTimeCurrent.SetWindowText(strTime);
+
+		// show current avg. speed
+		this->GetDlgItem(IDC_STATIC_ENCODER_SPEED_AVG)->SetWindowText(szSpeedEncoderAvg);
+		this->GetDlgItem(IDC_STATIC_READS_SPEED_AVG)->SetWindowText(szSpeedReadsAvg);
+		this->GetDlgItem(IDC_STATIC_WRITES_SPEED_AVG)->SetWindowText(szSpeedWritesAvg);
+
+		this->bCanUpdateWindow = true;
+	}
+}
+
+void CEncWAVtoAC3WorkDlg::CreateWorker()
+{
+	this->hThread = ::CreateThread(NULL,
+		0,
+		EncWorkThread,
+		&workParam,
+		0,
 		&this->dwThreadId);
 
-    if(this->hThread == NULL)
-    {
+	if (this->hThread == NULL)
+	{
 		::LogMessage(_T("Error: Failed to create worker thread!"));
 
-        // show critical error message
-        this->MessageBox(_T("Failed to create worker thread!"), 
-            _T("Fatal Error"), 
-            MB_OK | MB_ICONERROR);
-    }
-
-    return TRUE;
-}
-
-void CEncWAVtoAC3WorkDlg::OnClose()
-{
-    // stop file timer
-    KillTimer(WM_FILE_TIMER);
-
-    // stop total timer
-    KillTimer(WM_TOTAL_TIMER);
-
-    // stop encoding process and close work dialog
-    if(this->bTerminate == false)
-    {
-        this->bTerminate = true;
-        return;
-    }
-    else
-        this->EndDialog(IDOK);
-
-    CDialogEx::OnClose();
-}
-
-void CEncWAVtoAC3WorkDlg::OnDestroy()
-{
-    CDialogEx::OnDestroy();
-
-    if(this->hThread != NULL)
-    {
-        ::TerminateThread(this->hThread, 0);
-        ::CloseHandle(this->hThread);
-        this->hThread = NULL;
-    }
-}
-
-void CEncWAVtoAC3WorkDlg::OnTimer(UINT_PTR nIDEvent)
-{
-    switch(nIDEvent)
-    {
-    case WM_TOTAL_TIMER:
-        {
-            TCHAR strTime[32] = _T("");
-            m_ElapsedTimeTotal += 0.25;
-
-            // get time in format hh:mm:ss
-            if(m_ElapsedTimeTotal <= 59)
-            {
-                _stprintf(strTime, _T("Total elapsed time: 00:00:%02u\0"), 
-                    (unsigned long) m_ElapsedTimeTotal); // ss
-            }
-            else if(m_ElapsedTimeTotal <= 3599)
-            {
-                _stprintf(strTime, _T("Total elapsed time: 00:%02u:%02u\0"),
-                    ((unsigned long) m_ElapsedTimeTotal / 60), // mm
-                    ((unsigned long) m_ElapsedTimeTotal % 60)); // ss
-            }
-            else
-            {
-                _stprintf(strTime, _T("Total elapsed time: %02u:%02u:%02u\0"), 
-                    ((unsigned long) m_ElapsedTimeTotal / 60) / 60, // hh
-                    ((unsigned long) m_ElapsedTimeTotal / 60) % 60, // mm
-                    ((((unsigned long) m_ElapsedTimeTotal / 60) % 60) * 60) % 60); // ss
-            }
-
-            // write to dialog
-            if(this->bCanUpdateWindow == true)
-            {
-                this->bCanUpdateWindow = false;
-                m_StcTimeTotal.SetWindowText(strTime);
-                this->bCanUpdateWindow = true;
-            }
-
-            return;
-        }
-        break;
-
-    case WM_FILE_TIMER:
-        {
-            TCHAR strTime[32] = _T("");
-            m_ElapsedTimeFile += 0.25;
-
-            // get time in format hh:mm:ss
-            if(m_ElapsedTimeFile <= 59)
-            {
-                _stprintf(strTime, _T("Elapsed time: 00:00:%02u\0"), 
-                    (unsigned long) m_ElapsedTimeFile); // ss
-            }
-            else if(m_ElapsedTimeFile <= 3599)
-            {
-                _stprintf(strTime, _T("Elapsed time: 00:%02u:%02u\0"),
-                    ((unsigned long) m_ElapsedTimeFile / 60), // mm
-                    ((unsigned long) m_ElapsedTimeFile % 60)); // ss
-            }
-            else
-            {
-                _stprintf(strTime, _T("Elapsed time: %02u:%02u:%02u\0"), 
-                    ((unsigned long) m_ElapsedTimeFile / 60) / 60, // hh
-                    ((unsigned long) m_ElapsedTimeFile / 60) % 60, // mm
-                    ((((unsigned long) m_ElapsedTimeFile / 60) % 60) * 60) % 60); // ss
-            }
-
-            // write to dialog
-            if(this->bCanUpdateWindow == true)
-            {
-                this->bCanUpdateWindow = false;
-
-				// show current time
-                m_StcTimeCurrent.SetWindowText(strTime);
-
-				// show current avg. speed
-				this->GetDlgItem(IDC_STATIC_ENCODER_SPEED_AVG)->SetWindowText(szSpeedEncoderAvg);
-				this->GetDlgItem(IDC_STATIC_READS_SPEED_AVG)->SetWindowText(szSpeedReadsAvg);
-				this->GetDlgItem(IDC_STATIC_WRITES_SPEED_AVG)->SetWindowText(szSpeedWritesAvg);
-
-                this->bCanUpdateWindow = true;
-            }
-
-            return;
-        }
-        break;
-    };
-
-    CDialogEx::OnTimer(nIDEvent);
-}
-
-void CEncWAVtoAC3WorkDlg::OnBnClickedCancel()
-{
-    // stop encoding process and close work dialog
-    if(this->bTerminate == false)
-    {
-        this->bTerminate = true;
-    }
-    else
-    {
-        this->EndDialog(IDOK);
-    }
-
-    // OnCancel();
+		// show critical error message
+		this->MessageBox(_T("Failed to create worker thread!"),
+			_T("Fatal Error"),
+			MB_OK | MB_ICONERROR);
+	}
 }
