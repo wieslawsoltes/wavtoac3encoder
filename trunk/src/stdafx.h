d2d36d42c557eb22ba89b124afdd375310a01a56
wieslaw.soltes@gmail.com <wieslaw.soltes@gmail.com@be961cae-a4ca-11dd-adfd-a1ec4d499d49>
2014-01-26 17:57:18 +0000

    Updated program version

--- trunk/src/stdafx.h
+++ trunk/src/stdafx.h
@@ -25,7 +25,7 @@
 //
 
 // define program version
-#define ENCWAVTOAC3_VERSION _T("5.3")
+#define ENCWAVTOAC3_VERSION _T("5.4")
 
 // default preset name
 #define DEFAULT_PRESET_NAME (HaveLangStrings() ? GetLangString(0x00207001) : _T("Default"))
