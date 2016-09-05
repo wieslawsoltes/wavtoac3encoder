0285f3a155c205b12ff45a7b57d7ad02b4952334
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-09-05 19:09:08 +0200

    Updated package script

--- scripts/Package.cmd
+++ scripts/Package.cmd
@@ -1,9 +1,7 @@
 @echo off
 setlocal
-
-@call "PackageBinaries.cmd"
-@call "PackageBinariesAMD64.cmd"
-@call "PackageSources.cmd"
-@call "PackageInstallers.cmd"
-
+@call PackageBinaries.cmd
+@call PackageBinariesAMD64.cmd
+@call PackageSources.cmd
+@call PackageInstallers.cmd
 endlocal
