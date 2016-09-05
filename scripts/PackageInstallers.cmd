1815465edbad7e81af2bb046bb76d86999b0a96c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-09-05 19:27:43 +0200

    Use command-line compiler

--- scripts/PackageInstallers.cmd
+++ scripts/PackageInstallers.cmd
@@ -1,6 +1,6 @@
 @echo on
 setlocal
-set IssCompiler32=c:\Program Files (x86)\Inno Setup 5\Compil32.exe
-"%IssCompiler32%" /cc "SetupScript.iss"
-"%IssCompiler32%" /cc "SetupScriptAMD64.iss"
+set IssCompiler=c:\Program Files (x86)\Inno Setup 5\ISCC.exe
+"%IssCompiler%" "SetupScript.iss"
+"%IssCompiler%" "SetupScriptAMD64.iss"
 endlocal
