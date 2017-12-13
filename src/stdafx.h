f15726dd5ec1f9f9479ff14e7d7732a78e6e0eee
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-12-13 08:44:17 +0100

    Update stdafx.h

--- src/stdafx.h
+++ src/stdafx.h
@@ -1,28 +1,6 @@
-﻿//
-// WAV to AC3 Encoder
-// Copyright (C) 2007-2017 Wiesław Šoltés <wieslaw.soltes@gmail.com>
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; version 2 of the License.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//
-
-#pragma once
+﻿#pragma once
 
 #include "version.h"
-
-//
-// DEFAULTS
-//
 
 // define program version
 #define ENCWAVTOAC3_VERSION _T(VER_FILE_VERSION_SHORT_STR)
@@ -158,20 +136,3 @@ extern "C" {
 #if defined(__cplusplus)
 }
 #endif
-
-//
-// LOG
-//
-
-typedef struct TLogCtx
-{
-    bool bInit;
-    CString szLogFileName;
-    CFile fp;
-} LogCtx;
-
-void LogFile(CString szLogFileName);
-bool LogOpen();
-bool LogClose();
-bool LogMessage(CString szMessage);
-void LogAnsiMessage(const PTCHAR pszMessage, const char* msg);
