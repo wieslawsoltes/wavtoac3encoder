4da2273daed4cf29a90b56d80ef464016ba831eb
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-12-13 08:34:59 +0100

    Update MainApp.h

--- src/MainApp.h
+++ src/MainApp.h
@@ -1,95 +1,28 @@
-﻿//
-// WAV to AC3 Encoder
-// Copyright (C) 2007-2017 Wiesław Šoltés <wieslaw.soltes@gmail.com>
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; version 2 of the License.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//
-
-#pragma once
+﻿#pragma once
 
 #ifndef __AFXWIN_H__
 #error "include 'StdAfx.h' before including this file for PCH"
 #endif
 
 #include "res\resource.h"
-
 #include "configuration\Language.h"
 
-void ShowCommandLineHelp(HWND hWnd);
-
-typedef class TCommandLineOpt
+class CLangManager
 {
 public:
-    TCommandLineOpt()
-    {
-        this->bHelp = false;
-
-        this->bEncodeAndExit = false;
-
-        this->bSaveConfig = false;
-        this->bHaveSaveConfig = false;
-
-        this->szInputFile = _T("");
-        this->bHaveInputFile = false;
-
-        this->szOutputFile = _T("");
-        this->bHaveOutputFile = false;
-
-        this->szOutputPath = _T("");
-        this->bHaveOutputPath = false;
-
-        this->szLoadConfig = _T("");
-        this->bHaveLoadConfig = false;
-
-        this->szLoadPresets = _T("");
-        this->bHaveLoadFiles = false;
-
-        this->szLoadFiles = _T("");
-        this->bHaveLoadPresets = false;
-
-        this->szLoadEngines = _T("");
-        this->bHaveLoadEngines = false;
-    }
+    LangMap_t *m_Lang;
+    LangList_t m_LangLst;
+    CString m_szLangFileName = _T("");
+    BOOL m_bHaveLang = FALSE;
+    int m_nLangId = -1;
 public:
-    bool bHelp;
+    bool LoadLangConfig(CString &szFileName);
+    bool SaveLangConfig(CString &szFileName);
 public:
-    bool bEncodeAndExit;
-public:
-    bool bSaveConfig;
-    bool bHaveSaveConfig;
-public:
-    CString szInputFile;
-    bool bHaveInputFile;
-public:
-    CString szOutputFile;
-    bool bHaveOutputFile;
-public:
-    CString szOutputPath;
-    bool bHaveOutputPath;
-public:
-    CString szLoadConfig;
-    bool bHaveLoadConfig;
-public:
-    CString szLoadFiles;
-    bool bHaveLoadFiles;
-public:
-    CString szLoadPresets;
-    bool bHaveLoadPresets;
-public:
-    CString szLoadEngines;
-    bool bHaveLoadEngines;
-} CommandLineOpt;
+    void LoadLangStrings();
+    BOOL HaveLangStrings();
+    CString& GetLangString(int id);
+}
 
 class CEncWAVtoAC3App : public CWinAppEx
 {
@@ -100,14 +33,6 @@ public:
     virtual BOOL InitInstance();
     DECLARE_MESSAGE_MAP()
 public:
-    void InitLog();
-public:
-    LangMap_t *m_Lang;
-    LangList_t m_LangLst;
-    CString m_szLangFileName = _T("");
-    BOOL m_bHaveLang = FALSE;
-    int m_nLangId = -1;
-public:
     bool m_bIsPortable = true;
     CString m_szPresetsFilePath;
     CString m_szConfigFilePath;
@@ -117,11 +42,5 @@ public:
     CString m_szLogFilePath;
 };
 
+extern CLangManager theLangManager;
 extern CEncWAVtoAC3App theApp;
-
-bool LoadLangConfig(CString &szFileName);
-bool SaveLangConfig(CString &szFileName);
-
-extern void LoadLangStrings();
-extern BOOL HaveLangStrings();
-extern CString& GetLangString(int id);
