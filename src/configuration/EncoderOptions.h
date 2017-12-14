9cacbc2339b008395781a35d26dab27b16c82028
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-12-14 09:08:24 +0100

    Update EncoderOptions.h

--- src/configuration/EncoderOptions.h
+++ src/configuration/EncoderOptions.h
@@ -157,28 +157,95 @@ public:
     const static int nNumSupportedInputExt = 8;
     const static int nNumSupportedOutputExt = 1;
 public:
-    static LPTSTR szCurrentPresetsVersion;
+    static constexpr LPTSTR szCurrentPresetsVersion = _T("1.1.0.0");
     static constexpr int nValidCbrBitrates[nNumValidCbrBitrates]
     {
         0, 32, 40, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, 384, 448, 512, 576, 640
     };
-    static CChannelConfig ccAften[nNumChannelConfigAften];
-    static LPTSTR szRawSampleFormats[nNumRawSampleFormats];
+    static constexpr CChannelConfig ccAften[nNumChannelConfigAften]
+    {
+        { 0, 0, _T("1+1") },
+        { 1, 0, _T("1/0") },
+        { 2, 0, _T("2/0") },
+        { 3, 0, _T("3/0") },
+        { 4, 0, _T("2/1") },
+        { 5, 0, _T("3/1") },
+        { 6, 0, _T("2/2") },
+        { 7, 0, _T("3/2") },
+        { 0, 1, _T("1+1") },
+        { 1, 1, _T("1/0") },
+        { 2, 1, _T("2/0") },
+        { 3, 1, _T("3/0") },
+        { 4, 1, _T("2/1") },
+        { 5, 1, _T("3/1") },
+        { 6, 1, _T("2/2") },
+        { 7, 1, _T("3/2") }
+    };
+    static constexpr LPTSTR szRawSampleFormats[nNumRawSampleFormats]
+    {
+        (LPTSTR)(LPCTSTR)(DEFAULT_TEXT_IGNORED),
+        _T("u8"),
+        _T("s8"),
+        _T("s16_le"),
+        _T("s16_be"),
+        _T("s20_le"),
+        _T("s20_be"),
+        _T("s24_le"),
+        _T("s24_be"),
+        _T("s32_le"),
+        _T("s32_be"),
+        _T("float_le"),
+        _T("float_be"),
+        _T("double_le"),
+        _T("double_be")
+    };
     static CEncoderOptions encOpt[CEncoderPreset::nNumEncoderOptions];
-    static CString pszGroups[nNumEncoderOptionsGroups];
-    static CString szCbrOption;
-    static CString szVbrOption;
-    static CString szThreadsOption;
-    static CString szSimdOption;
-    static CString szRawSampleFormatOption;
-    static CString szRawSampleRateOption;
-    static CString szRawChannelsOption;
-    static TCHAR szSupportedInputExt[nNumSupportedInputExt][8];
-    static int nSupportedInputFormats[nNumSupportedInputExt];
-    static TCHAR szSupportedOutputExt[nNumSupportedOutputExt][8];
-    static int FindValidBitratePos(const int nBitrate);
+    static constexpr CString pszGroups[nNumEncoderOptionsGroups]
+    {
+        _T("Encoding options"),
+        _T("Bitstream info metadata"),
+        _T("Dynamic range compression and dialog normalization"),
+        _T("Input options"),
+        _T("Input filters"),
+        _T("Alternate bit stream syntax")
+    };
+    static constexpr CString szCbrOption = _T("-b");
+    static constexpr CString szVbrOption = _T("-q");
+    static constexpr CString szThreadsOption = _T("-threads");
+    static constexpr CString szSimdOption = _T("-nosimd");
+    static constexpr CString szRawSampleFormatOption = _T("-raw_fmt");
+    static constexpr CString szRawSampleRateOption = _T("-raw_sr");
+    static constexpr CString szRawChannelsOption = _T("-raw_ch");
+    static constexpr TCHAR szSupportedInputExt[nNumSupportedInputExt][8]
+    {
+        _T("wav"),
+        _T("pcm"),
+        _T("raw"),
+        _T("bin"),
+        _T("aiff"),
+        _T("aif"),
+        _T("aifc"),
+        #ifndef DISABLE_AVISYNTH
+        _T("avs")
+        #endif
+    };
+    static constexpr int nSupportedInputFormats[nNumSupportedInputExt]
+    {
+        PCM_FORMAT_WAVE,
+        PCM_FORMAT_RAW,
+        PCM_FORMAT_RAW,
+        PCM_FORMAT_RAW,
+        PCM_FORMAT_AIFF,
+        PCM_FORMAT_AIFF,
+        PCM_FORMAT_CAFF,
+    };
+    static constexpr TCHAR szSupportedOutputExt[nNumSupportedOutputExt][8]
+    {
+        _T("ac3")
+    };
 public:
     static void InitEncoderOptions();
+    static int FindValidBitratePos(const int nBitrate);
     static int FindOptionIndex(CString szOption);
     static void ResetEncoderOptionsLists();
     static void ParseEncoderPreset(CEncoderPreset &preset, ConfigList_t &clTmp);
