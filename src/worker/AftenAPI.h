3d671550e0bc6da2a5c59e38f33f9d903d5ee825
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-12-15 10:44:54 +0100

    Update AftenAPI.h

--- src/worker/AftenAPI.h
+++ src/worker/AftenAPI.h
@@ -22,16 +22,6 @@ typedef void(*lpLibAften_aften_remap_wav_to_a52)(void *samples, int n, int ch, A
 typedef void(*lpLibAften_aften_remap_mpeg_to_a52)(void *samples, int n, int ch, A52SampleFormat fmt, int acmod);
 typedef FloatType(*lpLibAften_aften_get_float_type)(void);
 
-const LPCSTR pszLibAften_aften_get_version = "aften_get_version";
-const LPCSTR pszLibAften_aften_set_defaults = "aften_set_defaults";
-const LPCSTR pszLibAften_aften_encode_init = "aften_encode_init";
-const LPCSTR pszLibAften_aften_encode_frame = "aften_encode_frame";
-const LPCSTR pszLibAften_aften_encode_close = "aften_encode_close";
-const LPCSTR pszLibAften_aften_wav_channels_to_acmod = "aften_wav_channels_to_acmod";
-const LPCSTR pszLibAften_aften_remap_wav_to_a52 = "aften_remap_wav_to_a52";
-const LPCSTR pszLibAften_aften_remap_mpeg_to_a52 = "aften_remap_mpeg_to_a52";
-const LPCSTR pszLibAften_aften_get_float_type = "aften_get_float_type";
-
 class AftenAPI
 {
 public:
@@ -40,6 +30,7 @@ public:
 public:
     CString szLibPath;
     HMODULE hLibAften;
+public:
     lpLibAften_aften_get_version LibAften_aften_get_version;
     lpLibAften_aften_set_defaults LibAften_aften_set_defaults;
     lpLibAften_aften_encode_init LibAften_aften_encode_init;
@@ -53,63 +44,63 @@ public:
     bool OpenAftenAPI()
     {
         hLibAften = LoadLibrary(szLibPath);
-        if (hLibAften == NULL)
+        if (hLibAften == nullptr)
             return false;
 
-        LibAften_aften_get_version = (lpLibAften_aften_get_version)GetProcAddress(hLibAften, pszLibAften_aften_get_version);
-        if (LibAften_aften_get_version == NULL)
+        LibAften_aften_get_version = (lpLibAften_aften_get_version)GetProcAddress(hLibAften, "aften_get_version");
+        if (LibAften_aften_get_version == nullptr)
             return false;
 
-        LibAften_aften_set_defaults = (lpLibAften_aften_set_defaults)GetProcAddress(hLibAften, pszLibAften_aften_set_defaults);
-        if (LibAften_aften_set_defaults == NULL)
+        LibAften_aften_set_defaults = (lpLibAften_aften_set_defaults)GetProcAddress(hLibAften, "aften_set_defaults");
+        if (LibAften_aften_set_defaults == nullptr)
             return false;
 
-        LibAften_aften_encode_init = (lpLibAften_aften_encode_init)GetProcAddress(hLibAften, pszLibAften_aften_encode_init);
-        if (LibAften_aften_encode_init == NULL)
+        LibAften_aften_encode_init = (lpLibAften_aften_encode_init)GetProcAddress(hLibAften, "aften_encode_init");
+        if (LibAften_aften_encode_init == nullptr)
             return false;
 
-        LibAften_aften_encode_frame = (lpLibAften_aften_encode_frame)GetProcAddress(hLibAften, pszLibAften_aften_encode_frame);
-        if (LibAften_aften_encode_frame == NULL)
+        LibAften_aften_encode_frame = (lpLibAften_aften_encode_frame)GetProcAddress(hLibAften, "aften_encode_frame");
+        if (LibAften_aften_encode_frame == nullptr)
             return false;
 
-        LibAften_aften_encode_close = (lpLibAften_aften_encode_close)GetProcAddress(hLibAften, pszLibAften_aften_encode_close);
-        if (LibAften_aften_encode_close == NULL)
+        LibAften_aften_encode_close = (lpLibAften_aften_encode_close)GetProcAddress(hLibAften, "aften_encode_close");
+        if (LibAften_aften_encode_close == nullptr)
             return false;
 
-        LibAften_aften_wav_channels_to_acmod = (lpLibAften_aften_wav_channels_to_acmod)GetProcAddress(hLibAften, pszLibAften_aften_wav_channels_to_acmod);
-        if (LibAften_aften_wav_channels_to_acmod == NULL)
+        LibAften_aften_wav_channels_to_acmod = (lpLibAften_aften_wav_channels_to_acmod)GetProcAddress(hLibAften, "aften_wav_channels_to_acmod");
+        if (LibAften_aften_wav_channels_to_acmod == nullptr)
             return false;
 
-        LibAften_aften_remap_wav_to_a52 = (lpLibAften_aften_remap_wav_to_a52)GetProcAddress(hLibAften, pszLibAften_aften_remap_wav_to_a52);
-        if (LibAften_aften_remap_wav_to_a52 == NULL)
+        LibAften_aften_remap_wav_to_a52 = (lpLibAften_aften_remap_wav_to_a52)GetProcAddress(hLibAften, "aften_remap_wav_to_a52");
+        if (LibAften_aften_remap_wav_to_a52 == nullptr)
             return false;
 
-        LibAften_aften_remap_mpeg_to_a52 = (lpLibAften_aften_remap_mpeg_to_a52)GetProcAddress(hLibAften, pszLibAften_aften_remap_mpeg_to_a52);
-        if (LibAften_aften_remap_mpeg_to_a52 == NULL)
+        LibAften_aften_remap_mpeg_to_a52 = (lpLibAften_aften_remap_mpeg_to_a52)GetProcAddress(hLibAften, "aften_remap_mpeg_to_a52");
+        if (LibAften_aften_remap_mpeg_to_a52 == nullptr)
             return false;
 
-        LibAften_aften_get_float_type = (lpLibAften_aften_get_float_type)GetProcAddress(hLibAften, pszLibAften_aften_get_float_type);
-        if (LibAften_aften_get_float_type == NULL)
+        LibAften_aften_get_float_type = (lpLibAften_aften_get_float_type)GetProcAddress(hLibAften, "aften_get_float_type");
+        if (LibAften_aften_get_float_type == nullptr)
             return false;
 
         return true;
     }
     void CloseAftenAPI()
     {
-        if (hLibAften != NULL)
+        if (hLibAften != nullptr)
         {
             FreeLibrary(hLibAften);
             szLibPath = _T("");
-            hLibAften = NULL;
-            LibAften_aften_get_version = NULL;
-            LibAften_aften_set_defaults = NULL;
-            LibAften_aften_encode_init = NULL;
-            LibAften_aften_encode_frame = NULL;
-            LibAften_aften_encode_close = NULL;
-            LibAften_aften_wav_channels_to_acmod = NULL;
-            LibAften_aften_remap_wav_to_a52 = NULL;
-            LibAften_aften_remap_mpeg_to_a52 = NULL;
-            LibAften_aften_get_float_type = NULL;
+            hLibAften = nullptr;
+            LibAften_aften_get_version = nullptr;
+            LibAften_aften_set_defaults = nullptr;
+            LibAften_aften_encode_init = nullptr;
+            LibAften_aften_encode_frame = nullptr;
+            LibAften_aften_encode_close = nullptr;
+            LibAften_aften_wav_channels_to_acmod = nullptr;
+            LibAften_aften_remap_wav_to_a52 = nullptr;
+            LibAften_aften_remap_mpeg_to_a52 = nullptr;
+            LibAften_aften_get_float_type = nullptr;
         }
     }
 };
