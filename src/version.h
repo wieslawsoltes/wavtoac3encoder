3359f33c24ec3dda5cdce9561bd95acdb68ef551
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-12-15 10:41:01 +0100

    Update version.h

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
-#define VERSION_MAJOR               6
+#define VERSION_MAJOR               7
 #define VERSION_MINOR               0
 #define VERSION_REVISION            0
 #define VERSION_BUILD               0
