a688e8d59e0f465590a5b4c6d2ad06d07e516f49
wieslawsoltes <wieslaw.soltes@gmail.com>
2018-02-08 12:08:41 +0000

    Added time count class

---
+++ src/worker/TimeCount.cpp
@@ -0,0 +1,25 @@
+﻿#include "StdAfx.h"
+#include "TimeCount.h"
+
+namespace worker
+{
+    void CTimeCount::Start() 
+    {
+        this->tc.Start();
+    }
+
+    void CTimeCount::Stop() 
+    {
+        this->tc.Stop();
+    }
+
+    double CTimeCount::ElapsedMilliseconds() 
+    {
+        return this->tc.ElapsedTime().count();
+    }
+
+    std::wstring CTimeCount::Formatted() 
+    {
+        return util::CTimeCount::Format(this->tc.ElapsedTime());
+    }
+}
---
+++ src/worker/TimeCount.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include <string>
+#include "utilities\TimeCount.h"
+
+namespace worker
+{
+    class CTimeCount
+    {
+        util::CTimeCount tc;
+    public:
+        void Start();
+        void Stop();
+        double ElapsedMilliseconds();
+        std::wstring Formatted();
+    };
+}
