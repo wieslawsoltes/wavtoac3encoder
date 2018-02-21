c5db0c4a91bd8ef68180c3144fe2da19db6f09a6
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-02-21 20:15:43 +0100

    Added includes

--- src/cli/stdafx.h
+++ src/cli/stdafx.h
@@ -2,8 +2,23 @@
 
 #include "targetver.h"
 
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
 #include <stdio.h>
 #include <tchar.h>
 
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
+#include <string>
+#include <vector>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <thread>
+
+#include "logger\Log.h"
+#include "utilities\StringHelper.h"
+#include "utilities\TimeCount.h"
+#include "utilities\Utilities.h"
+#include "avs\src\Avs2Raw\Avs2Raw.h"
+#include "configuration\Configuration.h"
+#include "worker\Worker.h"
