c401f9e28c49e4b164a6da170071597b7d97724f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-02-21 18:15:49 +0100

    Added cli project

---
+++ src/cli/EncWAVtoAC3.CLI.cpp
@@ -0,0 +1,10 @@
+﻿#include "stdafx.h"
+
+#include "utilities\Utilities.h"
+#include "logger\Log.h"
+#include "configuration\Configuration.h"
+
+int _tmain(int arc, TCHAR *argv[])
+{
+    return 0;
+}
---
+++ src/cli/stdafx.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
